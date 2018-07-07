f3ea7f741530c2e6e4e929bcb36fcf8efff1b51f
root <953372680@qq.com>
2018-07-07 19:24:35 +0800

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-echo "===========进入git项目happymmall目录============="
-cd /developer/git-repository/mmall
+#!/bin/sh
+echo "===========进入git项目medicine目录============="
+cd /developer/git-repository/java_medicine
 
-echo "==========git切换分之到mmall-v1.0==============="
-git checkout mmall-v1.0
+echo "==========git切换分之到v1.0==============="
+git checkout v1.0
 
 echo "==================git fetch======================"
 git fetch
@@ -10,27 +11,20 @@ git fetch
 echo "==================git pull======================"
 git pull
 
-
 echo "===========编译并跳过单元测试===================="
 mvn clean package -Dmaven.test.skip=true
-
 
 echo "============删除旧的ROOT.war==================="
 rm /developer/apache-tomcat-7.0.73/webapps/ROOT.war
 
-
 echo "======拷贝编译出来的war包到tomcat下-ROOT.war======="
-cp /developer/git-repository/mmall/target/mmall.war  /developer/apache-tomcat-7.0.73/webapps/ROOT.war
-
+cp /developer/git-repository/java_medicine/target/java_medicine.war  /developer/apache-tomcat-7.0.73/webapps/ROOT.war
 
 echo "============删除tomcat下旧的ROOT文件夹============="
 rm -rf /developer/apache-tomcat-7.0.73/webapps/ROOT
 
-
-
 echo "====================关闭tomcat====================="
 /developer/apache-tomcat-7.0.73/bin/shutdown.sh
-
 
 echo "================sleep 10s========================="
 for i in {1..10}
@@ -38,7 +32,6 @@ do
 	echo $i"s"
 	sleep 1s
 done
-
 
 echo "====================启动tomcat====================="
 /developer/apache-tomcat-7.0.73/bin/startup.sh

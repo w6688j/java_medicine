45855dee6cd81fc197b7df7495772ad4212b19da
root <953372680@qq.com>
2018-07-07 19:08:40 +0800

    init

---
+++ src/main/java/com/medicine/controller/portal/UserController.java
@@ -0,0 +1,43 @@
+package com.medicine.controller.portal;
+
+import com.google.common.collect.Maps;
+import com.medicine.common.ServerResponse;
+import com.medicine.service.IFileService;
+import com.medicine.util.PropertiesUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * Created by wangjian
+ * User: root
+ * Date: 2018/7/7
+ * Time: 18:14
+ */
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    @Autowired
+    private IFileService iFileService;
+
+    @RequestMapping("upload.do")
+    @ResponseBody
+    public ServerResponse upload(@RequestParam(value = "upload_file", required = false) MultipartFile file, HttpServletRequest request) {
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        String targetFileName = iFileService.upload(file, path);
+        String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+        Map fileMap = Maps.newHashMap();
+        fileMap.put("uri", targetFileName);
+        fileMap.put("url", url);
+
+        return ServerResponse.createBySuccess(fileMap);
+    }
+}
